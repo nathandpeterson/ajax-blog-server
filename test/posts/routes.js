00682c6ec06849295d38d4a6431238e258de6e89
Wes Reid <bwreid@gmail.com>
2017-11-01 19:25:55 -0700

    Separate routes into diff describe blocks

--- test/posts/routes.js
+++ test/posts/routes.js
@@ -13,34 +13,40 @@ describe('post routes', function () {
           done()
         })
     })
+  })
 
+  describe('POST /', function () {
     it('should create a new post', function (done) {
       const post = { title: 'xxx', content: 'yyy' }
       chai.request(app)
-        .post('/posts')
-        .send(post)
-        .end((err, res) => {
-          expect(res.status).to.equal(201)
-          expect(res.body.post.id).to.be.ok
-          expect(res.body.post.title).to.deep.equal('xxx')
-          expect(res.body.post.content).to.deep.equal('yyy')
-          done()
-        })
+      .post('/posts')
+      .send(post)
+      .end((err, res) => {
+        expect(res.status).to.equal(201)
+        expect(res.body.post.id).to.be.ok
+        expect(res.body.post.title).to.deep.equal('xxx')
+        expect(res.body.post.content).to.deep.equal('yyy')
+        done()
+      })
     })
+  })
 
+  describe('GET /:id', function () {
     it('should retrieve a specific post', function (done) {
       const post = { id: uuid(), title: 'xxx', content: 'yyy' }
       fs.writeFileSync(db, JSON.stringify([ post ]))
 
       chai.request(app)
-        .get(`/posts/${post.id}`)
-        .end((err, res) => {
-          expect(res.status).to.equal(200)
-          expect(res.body.post).to.deep.equal(post)
-          done()
-        })
+      .get(`/posts/${post.id}`)
+      .end((err, res) => {
+        expect(res.status).to.equal(200)
+        expect(res.body.post).to.deep.equal(post)
+        done()
+      })
     })
+  })
 
+  describe('DELETE /:id', function () {
     it('should destroy a specific post', function (done) {
       const post = { id: uuid(), title: 'xxx', content: 'yyy' }
       fs.writeFileSync(db, JSON.stringify([ post ]))
